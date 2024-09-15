74a1a2d739f0508e7cbf02c25110eb4b03e4a755
Hans Llanos <hans.llanos@gmail.com>
2024-09-15 13:34:15 -0500

    instalacion de react prime y prime icons

--- miniproyecto/src/main.jsx
+++ miniproyecto/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import 'primeicons/primeicons.css';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
